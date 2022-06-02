64a30bf4a46ce5c9c240f84e40f5c8101dbf847a
Gustavo <gustavodarocha241@gmail.com>
2022-06-02 10:04:46 -0300

    requisitos 5 e 6 completos

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+const sum = {};
 
+function countAnimals(animal) {
+  if (animal === undefined) {
+    species.forEach((ani) => {
+      sum[ani.name] = ani.residents.length;
+    });
+    return sum;
+  }
+  if (animal.sex === 'male' || animal.sex === 'female') {
+    return species.find((obj) => obj.name === animal.specie).residents
+      .filter((object) => object.sex === animal.sex).length;
+  }
+  if (animal.sex === undefined) {
+    const animalName = species.find((animals) => animals.name === animal.specie).residents.length;
+    return animalName;
+  }
 }
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,17 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('confere se quando o parametro for count retorna o valor esperado', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('confere se quando o parametro for um objeto vazio retorna parametro invalido', () => {
+    const test = {};
+    expect(handlerElephants(test)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('confere se quando o parametro for averageAge retorna um valor proximo do esperado', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('caso o parametro seja location retorna a localozação dos elefantes', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+});
